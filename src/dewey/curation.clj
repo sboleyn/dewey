df6a61a6f0f0f5efb5a02cbaef7876d87125625b
Ian McEwen <mian@cyverse.org>
2016-12-01 12:14:39 -0700

    Move the rethrow out of the if, for repetitiveness' sake

--- src/dewey/curation.clj
+++ src/dewey/curation.clj
@@ -342,13 +342,11 @@
             (log/info "Attempted to index a non-existent iRODS entity. Most likely it was deleted after"
                       "this index message was created."))
           (catch JargonException e
-            (if (instance? IOException (.getCause e))
-              (do
-                (log/warn "Failed to connect to iRODs. Could not process route" routing-key
-                          "with message" msg ". Backing off.")
-                (irods-backoff) ; This will retry forever
-                (throw e))
-              (throw e))))
+            (when (instance? IOException (.getCause e))
+              (log/warn "Failed to connect to iRODs. Could not process route" routing-key
+                        "with message" msg ". Backing off.")
+              (irods-backoff)) ; This will retry forever
+            (throw e)))
         (log/warn (str "unknown routing key" routing-key "received with message" msg)))
       (let [consumetime (milliseconds-since consume-start)]
         (tc/with-logging-context {:amqp-total-time consumetime :dewey-consume-time @innertime}
